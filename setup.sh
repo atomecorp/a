9a0077afb5226ae5902a1858b737ed88c7370dcf
Jean-Eric Godard <jeezs@jeezs.net>
2025-05-13 19:46:48 +0200

    updated .sh file

--- setup.sh
+++ setup.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Vérifier si un nom d'application a été fourni
-if [ $# -eq 0 ]; then
-  echo "Erreur: Veuillez fournir un nom d'application."
-  echo "Usage: ./setup.sh nom_application"
-  exit 1
-fi
-
-APP_NAME=$1
-
-echo "Création de l'application Tauri: $APP_NAME"
-#!/bin/bash
-
 # Définir un nom d'application par défaut
 DEFAULT_APP_NAME="my_app"
 
@@ -25,16 +13,6 @@ fi
 
 echo "Création de l'application Tauri: $APP_NAME"
 
-# Utiliser create-tauri-app avec le template vanilla pour créer l'application
-# L'option --yes permet d'accepter automatiquement les valeurs par défaut
-npm create tauri-app@latest $APP_NAME -- --template vanilla --manager npm --yes
-
-# Accéder au répertoire de l'application
-cd $APP_NAME
-
-# Lancer le développement
-echo "Lancement de l'application en mode développement..."
-npm run tauri dev
 # Utiliser create-tauri-app avec le template vanilla pour créer l'application
 # L'option --yes permet d'accepter automatiquement les valeurs par défaut
 npm create tauri-app@latest $APP_NAME -- --template vanilla --manager npm --yes
