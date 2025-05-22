a13d75f2a2bd3a00d26880908e20528f26374843
Jean-Eric Godard <jeezs@jeezs.net>
2025-05-22 21:02:03 +0200

    Better parser integration now need to implement javascript generator

--- src/squirrel/run_application.js
+++ src/squirrel/run_application.js
@@ -1,26 +1,36 @@
 
-fetch("./application/example.sqr")
-    .then((res) => res.text())
-    .then((code) => {
-        runSquirrel(code)
-    })
-    .catch((err) => {
-        console.error("❌ Erreur :", err);
-    });
+// fetch("./application/example.sqr")
+//     .then((res) => res.text())
+//     .then((code) => {
+//         runSquirrel(code)
+//     })
+//     .catch((err) => {
+//         console.error("❌ Erreur :", err);
+//     });
+//
+//
+// fetch("./application/example.sqj")
+//     .then((res) => res.text())
+//     .then((code) => {
+//         runSquirrel(code)
+//     })
+//     .catch((err) => {
+//         console.error("❌ Erreur :", err);
+//     });
 
+window.addEventListener('DOMContentLoaded', () => {
+    // on attend que la lib soit dispo globalement
+    const checkReady = setInterval(() => {
+        if (typeof LibRubyParser !== 'undefined' && typeof LibRubyParser.parse === 'function') {
+            clearInterval(checkReady);
 
-fetch("./application/example.sqj")
-    .then((res) => res.text())
-    .then((code) => {
-        runSquirrel(code)
-    })
-    .catch((err) => {
-        console.error("❌ Erreur :", err);
-    });
-
-
-
-
-
-
-
+            fetch('../application/example.sqr')
+                .then(res => res.text())
+                .then(code => {
+                    const result = LibRubyParser.parse(code);  // appel direct à la lib globale
+                    console.log(result);
+                })
+                .catch(console.error);
+        }
+    }, 50);
+});
