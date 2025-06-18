36c16d4dcd69811996e41e8d421574505abfe5cf
Jean-Eric Godard <jeezs@jeezs.net>
2025-06-18 21:29:40 +0200

    🚀 Release v1.0.0 - CDN update
        - Updated squirrel.js bundle
        - Updated squirrel.min.js minified version
        - Ready for jsDelivr CDN

--- src/application/index.js
+++ src/application/index.js
@@ -14,33 +14,33 @@
 
 // import('./examples/matrix.js');
 // import('./examples/drag.js');
-// import('./examples/dragNdrop.js');
+import('./examples/dragNdrop.js');
 
 // import('./examples/unit.js');
 
 
-const monSpan = $('span', {
-// pas besoin de 'tag'
-id: 'test1',
-css: {
-backgroundColor: '#00f',
-display: 'block',
-position: 'absolute',
-marginLeft: '0',
-padding: '10px',
-color: 'white',
-margin: '10px',
-display: 'inline-block'
-},
-text: 'Je suis un SPAN ! 🎯'
-});
+// const monSpan = $('span', {
+// // pas besoin de 'tag'
+// id: 'test1',
+// css: {
+// backgroundColor: '#00f',
+// display: 'block',
+// position: 'absolute',
+// marginLeft: '0',
+// padding: '10px',
+// color: 'white',
+// margin: '10px',
+// display: 'inline-block'
+// },
+// text: 'Je suis un SPAN ! 🎯'
+// });
 
-makeDraggable(grab('test1'), {
-  onDragStart: (element, event) => { puts(`Début du drag sur l'élément ${element.id} !`); }
-});
-// grab('test1').left='100px';
+// makeDraggable(grab('test1'), {
+//   onDragStart: (element, event) => { puts(`Début du drag sur l'élément ${element.id} !`); }
+// });
+// // grab('test1').left='100px';
 
-const a=grab('test1');
-puts (a);
+// const a=grab('test1');
+// puts (a);
 
-a.style.left= '100px';
+// a.style.left= '100px';
