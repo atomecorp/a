4b222b35126819acbeb81aac4be510c3bfbdba2a
Jean-Eric Godard <jeezs@jeezs.net>
2025-06-18 13:54:33 +0200

    temporary test

--- src/application/index.js
+++ src/application/index.js
@@ -16,4 +16,31 @@
 // import('./examples/drag.js');
 // import('./examples/dragNdrop.js');
 
-import('./examples/unit.js');
+// import('./examples/unit.js');
+
+
+const monSpan = $('span', {
+// pas besoin de 'tag'
+id: 'test1',
+css: {
+backgroundColor: '#00f',
+display: 'block',
+position: 'absolute',
+marginLeft: '0',
+padding: '10px',
+color: 'white',
+margin: '10px',
+display: 'inline-block'
+},
+text: 'Je suis un SPAN ! 🎯'
+});
+
+makeDraggable(grab('test1'), {
+  onDragStart: (element, event) => { puts(`Début du drag sur l'élément ${element.id} !`); }
+});
+// grab('test1').left='100px';
+
+const a=grab('test1');
+puts (a);
+
+a.style.left= '100px';
