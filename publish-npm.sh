a66c9c723cfeedc7f4c2c6937b746582afe66617
Jean-Eric Godard <jeezs@jeezs.net>
2025-06-20 18:03:21 +0200

    NPM publication added

---
+++ publish-npm.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+# Script de publication NPM + unpkg
+# Usage: ./publish-npm.sh
+
+set -e
+
+# 1. Build avec rollup (config dans ./scripts_utils/rollup.config.npm.js)
+./node_modules/.bin/rollup -c ./scripts_utils/rollup.config.npm.js
+
+# 2. (Optionnel) Minification
+if command -v terser > /dev/null; then
+  terser ./dist/squirrel.js -o ./dist/squirrel.min.js --compress --mangle
+else
+  echo "terser non trouvé, minification ignorée. Installez-le avec: npm i -g terser"
+fi
+
+echo "Build terminé. Fichiers générés dans ./dist :"
+ls -lh ./dist/squirrel*.js
+
+echo ""
+read "REPLY?Publier sur NPM et unpkg maintenant ? (y/N): "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  npm publish --access public
+  PKG_NAME=$(node -p "require('./package.json').name")
+  PKG_VERSION=$(node -p "require('./package.json').version")
+  echo "\nUnpkg prêt :"
+  echo "https://unpkg.com/$PKG_NAME@$PKG_VERSION/dist/squirrel.js"
+  echo "https://unpkg.com/$PKG_NAME@$PKG_VERSION/dist/squirrel.min.js"
+else
+  echo "Publication NPM annulée. Les fichiers sont prêts dans ./dist."
+fi
