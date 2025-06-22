387c060871a154b3373d464829be433571a6eb37
Jean-Eric Godard <jeezs@jeezs.net>
2025-06-22 09:41:22 +0200

    vie bottom

--- src/application/vie/vie.js
+++ src/application/vie/vie.js
@@ -77,3 +77,24 @@ const menuOverlay = $('div', {
 menuButton.addEventListener('click', () => {
   menuOverlay.style.display = menuOverlay.style.display === 'none' ? 'flex' : 'none';
 });
+
+
+const bottomBar = $('div', {
+  id: 'bottom-bar',
+  css: {
+    backgroundColor: 'rgb(68, 142, 220)',
+    marginLeft: '0',
+    color: 'white',
+    bottom: '0',
+    left: '0',
+    position: 'fixed',
+    height: '39px',
+    width: '100%',
+    textAlign: 'center',
+    fontSize: '20px',
+    fontWeight: 'bold',
+    borderBottom: '2px solid rgb(68, 142, 220)',
+    display: 'block'
+  },
+  text: 'Je suis la vie !'
+});
