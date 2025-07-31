575deb385cb13c4e9fb5fb7150fe1f6e9c25d3e6
Jean-Eric Godard <jeezs@jeezs.net>
2025-07-31 13:16:17 +0200

    still cleaning

--- src/application/lyrix/src/constants.js
+++ src/application/lyrix/src/constants.js
@@ -2,12 +2,9 @@
 export const CONSTANTS = {
     // Audio configuration
     AUDIO: {
-        BASE_PATH: 'assets/audios/',
+        BASE_PATH: './assets/audios/',
         BASE_URL: 'http://localhost:3001/assets/audios/',
-        DEMO_FILES: {
-            darkbox: 'darkbox.mp3',
-            digitalDreams: 'digital_dreams.mp3'
-        }
+
     },
     
     // Storage keys
@@ -44,68 +41,5 @@ export const CONSTANTS = {
         AUDIO_MIME: ['audio/', 'video/']
     },
     
-    // Demo songs data
-    DEMO_SONGS: {
-        DARKBOX: {
-            title: "The Darkbox",
-            artist: "Atome Artist",
-            album: "Demo Album",
-            audioFile: "darkbox.mp3",
-            lyrics: [
-                { time: 0, text: "Spread the words", type: "vocal" },
-                { time: 2000, text: "That'll burn your mind", type: "vocal" },
-                { time: 4000, text: "Seal your eyes", type: "vocal" },
-                { time: 6000, text: "Shut your ears", type: "vocal" },
-                { time: 8000, text: "Swallow this and dive inside", type: "vocal" },
-                { time: 10000, text: "dive inside", type: "vocal" },
-                { time: 11000, text: "dive inside", type: "vocal" },
-                { time: 12000, text: "dive inside", type: "vocal" },
-                { time: 14000, text: "", type: "instrumental" },
-                { time: 16000, text: "The darkbox...", type: "chorus" },
-                { time: 18000, text: "", type: "instrumental" },
-                { time: 20000, text: "Do you wanna be scared", type: "vocal" },
-                { time: 22000, text: "No real fun won't begin", type: "vocal" },
-                { time: 24000, text: "Stay away from what is there", type: "vocal" },
-                { time: 26000, text: "", type: "instrumental" },
-                { time: 28000, text: "Close your mind", type: "vocal" },
-                { time: 30000, text: "Widely shut,", type: "vocal" },
-                { time: 32000, text: "You won't see it's a trap", type: "vocal" },
-                { time: 34000, text: "A golden coffin for your mind", type: "vocal" },
-                { time: 36000, text: "", type: "instrumental" },
-                { time: 38000, text: "The darkbox", type: "chorus" },
-                { time: 40000, text: "", type: "instrumental" },
-                { time: 42000, text: "Ghost box, don't get inside this dark box", type: "vocal" },
-                { time: 45000, text: "No satisfaction out of the box", type: "vocal" },
-                { time: 48000, text: "", type: "instrumental" },
-                { time: 50000, text: "Ghost box, stay away from this dark box", type: "vocal" },
-                { time: 53000, text: "Destroy this fuckin' Pandora's box", type: "vocal" },
-                { time: 56000, text: "", type: "instrumental" },
-                { time: 58000, text: "Ghost box, don't get inside this dark box", type: "vocal" },
-                { time: 61000, text: "No satisfaction out of the box", type: "vocal" },
-                { time: 64000, text: "", type: "instrumental" },
-                { time: 66000, text: "Ghost box, don't get inside this dark box", type: "vocal" },
-                { time: 69000, text: "Smash that nightmare box", type: "vocal" },
-                { time: 72000, text: "", type: "instrumental" },
-                { time: 74000, text: "Smash that nightmare box", type: "vocal" },
-                { time: 76000, text: "Smash that nightmare box", type: "vocal" }
-            ]
-        },
-        DIGITAL_DREAMS: {
-            title: "Digital Dreams",
-            artist: "Cyber Collective",
-            album: "Electronic Visions",
-            audioFile: "digital_dreams.mp3",
-            lyrics: [
-                { time: 0, text: "In the neon lights we find", type: "vocal" },
-                { time: 3000, text: "Digital dreams of a different kind", type: "vocal" },
-                { time: 6000, text: "Circuits dancing in the night", type: "vocal" },
-                { time: 9000, text: "Electric souls burning bright", type: "vocal" },
-                { time: 12000, text: "", type: "instrumental" },
-                { time: 15000, text: "Download my heart", type: "chorus" },
-                { time: 18000, text: "Upload your soul", type: "chorus" },
-                { time: 21000, text: "In this digital world", type: "chorus" },
-                { time: 24000, text: "We lose control", type: "chorus" }
-            ]
-        }
-    }
+
 };
