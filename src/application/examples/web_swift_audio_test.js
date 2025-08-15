a575c0317ac43a9b0c177e642e4b5f1c0304b681
Jean-Eric Godard <jeezs@jeezs.net>
2025-08-15 13:09:07 +0200

    bck

---
+++ src/application/examples/web_swift_audio_test.js
@@ -0,0 +1,13 @@
+    $('audio', {
+      id: 'riffPlayer',
+      attrs: {
+        src: 'assets/audios/riff.m4a',
+        controls: true
+      },
+      css: {
+        margin: '20px',
+        width: '300px',
+        outline: 'none',
+        borderRadius: '6px'
+      }
+    });
--- src/application/index.js
+++ src/application/index.js
@@ -42,15 +42,21 @@
 
 
 //  import('./examples/audio_basic.js');
+////////////////////
+// import('./lyrix/index.js');
+// import('./ios_file_lister.js');
+// import('./ios_file_writer.js');
+// import('./ios_web_view_filereader.js');
+// import('./ios_audio_bridge.js');
+// import('./auv3_file_lister.js');
+// import('./auv3_file_writer.js');
+// import('./test_and_rd.js');
+////////////////////
 
-import('./lyrix/index.js');
-import('./ios_file_lister.js');
-import('./ios_file_writer.js');
-import('./ios_web_view_filereader.js');
-import('./ios_audio_bridge.js');
-import('./auv3_file_lister.js');
-import('./auv3_file_writer.js');
-import('./test_and_rd.js');
+
+import('./examples/web_swift_audio_test.js');
+
+
 
 // import('./examples/ios_apis.js');
 
