1f11e02242fb394fd8cb3e5bd5656b4ce29af8bd
Jean-Eric Godard <jeezs@jeezs.net>
2025-08-18 13:16:33 +0200

    better audio player

--- src/application/index.js
+++ src/application/index.js
@@ -58,10 +58,10 @@ import('./examples/web_swift_audio_test.js');
 
 // import('./examples/drag_2_ios_local.js');
 
-import('./examples/auv3file_browser.js');
+// import('./examples/auv3file_browser.js');
 
 
-import('./examples/swift_server.js');
+// import('./examples/swift_server.js');
 
 
 
