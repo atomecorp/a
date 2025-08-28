1feef41880cdfcd2da1913c68c3b61e3ecf77c44
Jean-Eric Godard <jeezs@jeezs.net>
2025-08-28 10:10:06 +0200

    more cleanup

--- src/application/index.js
+++ src/application/index.js
@@ -44,16 +44,12 @@
 //  import('./examples/audio_basic.js');
 ////////////////////
 // import('./lyrix/index.js');
-import('./examples/iOS_app_launcher.js');
-// import('./ios_audio_bridge.js');
-// import('./ios_file_lister.js');
-// import('./ios_file_reader.js');
-// import('./ios_file_writer.js');
-// import('./ios_web_view_filereader.js');
+// import('./examples/iOS_app_launcher.js');
+import('./examples/ios_audio_bridge.js');
 
-// import('./auv3_file_lister.js');
-// import('./auv3_file_writer.js');
-// import('./test_and_rd.js');
+
+
+
 
 ////
 // import('./examples/web_swift_audio_test.js');
