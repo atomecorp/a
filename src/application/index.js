c6f0e99ffec62517de76533a0c09e67b9b70346e
Jean-Eric Godard <jeezs@jeezs.net>
2025-11-06 13:57:42 +0100

    now console can be opened programatically

--- src/application/index.js
+++ src/application/index.js
@@ -90,8 +90,7 @@
 
 // createBasicConsole();
 
-
-
+openConsole();
 
 
 
--- src/squirrel/default/shortcuts.js
+++ src/squirrel/default/shortcuts.js
@@ -1,12 +1,12 @@
 
-  function createBasicConsole() {
+function createBasicConsole() {
   const console1 = Console({
     title: 'Debug Console',
     position: { x: 50, y: 50 },
     size: { width: 500, height: 350 },
     template: 'dark_theme'
   });
-  
+
   console1.show();
   return console1;
 }
@@ -14,7 +14,9 @@
 
 
 
-  const myFct = function atest(key){
-createBasicConsole();
-     };
-  shortcut('alt-c', myFct);
+const openConsole = function atest(key) {
+  createBasicConsole();
+};
+shortcut('alt-c', openConsole);
+
+window.openConsole = openConsole;
