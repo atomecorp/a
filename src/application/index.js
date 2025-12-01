ed0ebc4456fa0beb10a369100dd8ed77a8c8ec8e
Jean-Eric Godard <jeezs@jeezs.net>
2025-12-01 22:35:03 +0100

    hey!

--- src/application/index.js
+++ src/application/index.js
@@ -48,7 +48,7 @@
 
 //  import('./examples/audio_basic.js');
 ////////////////////
-puts('Yes!!');
+puts('Yes yes yes!!');
 // import('./aBox/index.js');
 // import('./examples/user_creation.js');
 import('./examples/update.js');
