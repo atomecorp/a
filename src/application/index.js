e0c88dabfd5212eecedab4ea83b74049b3bb8f17
Jean-Eric Godard <jeezs@jeezs.net>
2025-12-01 16:34:53 +0100

    tryout

--- src/application/index.js
+++ src/application/index.js
@@ -49,12 +49,12 @@
 //  import('./examples/audio_basic.js');
 ////////////////////
 
-import('./aBox/index.js');
+// import('./aBox/index.js');
 // import('./examples/user_creation.js');
 // import('./examples/update.js');
 
 
-// import('./lyrix/index.js');
+import('./lyrix/index.js');
 // import('./examples/input_file.js');
 // import('./examples/iOS_app_launcher.js');
 // import('./examples/ios_audio_bridge.js');
